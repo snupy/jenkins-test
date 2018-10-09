f09f71e64d1e626f13a654de0ce13e5eda48e8fc
malik <gshmalik@gmail.com>
2018-10-10 00:18:43 +0300

    minor fix

--- src/main/java/ru/malik/Application.java
+++ src/main/java/ru/malik/Application.java
@@ -1,7 +1,7 @@
 package ru.malik;
 
 public class Application {
-    public static void main(String[] args){
-        System.out.println("Рello world");
+    public static void main(String[] args) {
+        System.out.println("Рello world1");
     }
 }
